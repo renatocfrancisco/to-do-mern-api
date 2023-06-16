b410d48a04fe55914a37ed9ef48f4e6a6bb1aa1e
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-16 16:34:48 -0300

    chore: :bulb: remover comentário em admin.js

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,5 +1,3 @@
-// Purpose: Admin middleware, checks if user.admin is true
-
 const admin = (req, res, next) => {
   if (!req.user.admin) {
     return res.status(403).json({ msg: 'Access denied. Not an admin' })
