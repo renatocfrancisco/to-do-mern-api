e494c4bf75239bbbcdf51b577215e040a2e5239c
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-18 22:06:20 -0300

    chore: :art: formatar app.js

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-const express = require("express");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const compression = require("compression");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { db } = require("./config/db");
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { db } = require('./config/db')
 
-const routes = require("./routes/index");
-const user = require("./routes/user");
-const task = require("./routes/task");
+const routes = require('./routes/index')
+const user = require('./routes/user')
+const task = require('./routes/task')
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Successful MongoDB connection!");
-});
-db.on("disconnected", () => {
-  console.log("MongoDB disconnected!");
-});
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => {
+  console.log('Successful MongoDB connection!')
+})
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected!')
+})
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(helmet(), compression(), morgan("dev"), express.json());
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+app.use(cors(), helmet(), compression(), morgan('dev'), express.json())
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (_req, res) => {
-  res.send("Hello World! This is the root route of to-do-mern-api");
-});
+app.get('/', (_req, res) => {
+  res.send('Hello World! This is the root route of to-do-mern-api')
+})
 
-routes(app);
-app.use("/user", user);
-app.use("/task", task);
+routes(app)
+app.use('/user', user)
+app.use('/task', task)
 
-module.exports = app;
+module.exports = app
