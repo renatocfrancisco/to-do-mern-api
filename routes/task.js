c0666f49846abe457ace0ed970892adad7a935aa
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-07-05 12:37:59 -0300

    feat: :sparkles: ratelimit em rotas

--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,15 @@
 const jwt = require('../middlewares/jwt')
 const router = require('express').Router()
 const TaskController = require('../controllers/task')
+const rateLimit = require('express-rate-limit')
 
-router.use(jwt)
+const limiter = rateLimit({
+    windowMs: 1*60*1000,
+    max: 50,
+    message: 'Max requests reached. Please try again later.'
+});
+
+router.use(jwt, limiter)
 
 router.get('/', TaskController.getTasks)
 router.get('/:id', TaskController.getTask)
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,15 @@ const jwt = require('../middlewares/jwt')
 const admin = require('../middlewares/admin')
 const router = require('express').Router()
 const UserController = require('../controllers/user')
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+    windowMs: 1*60*1000,
+    max: 50,
+    message: 'Max requests reached. Please try again later.'
+});
+
+router.use(limiter)
 
 router.get('/', jwt, admin, UserController.getUsers)
 router.get('/:id', jwt, admin, UserController.getUser)
